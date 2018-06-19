2f7c11b137cdbff927b78846572a6926b7d3bb3b
Monica <monica_k@gwu.edu>
2018-06-19 08:39:32 -0400

    Testing header files

--- index.js
+++ index.js
@@ -1,19 +1,50 @@
 <html>
-<head>
 <title>Atoms to Astronauts</title>
-<link rel="stylesheet" href="style.css">
-<script src="https://code.jquery.com/jquery-1.10.2.js"></script>
-<script> 
-$(function(){
-  $("#header").load("header.html"); 
-
-});
-</script> 
+<head>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-<div id="header"></div>
-<!--Remaining section-->
-<h1> Our Mission </h1>
+    <div class = top-email>
+        atomstoastronauts@gmail.com
+    </div>
+    <div class = "logo">
+        <a href = "/">
+            <img src="logo_transparent.png" alt = "a2a-logo">
+        </a>
+    </div>
+    
+    <div class="navbar">
+        <a href="/">Home</a>
+        <a href="/about">About</a>
+        <div class="dropdown">
+            <button class="dropbtn">Projects
+                <i class="fa fa-caret-down"></i>
+            </button>
+            <div class="dropdown-content">
+                <a href="/past-projects">Past Projects</a>
+                <a href="/project-ideas">Projects to Try at Home</a>
+            </div>
+        </div>
+        <div class="dropdown">
+                <button class="dropbtn">Get Involved
+                    <i class="fa fa-caret-down"></i>
+                </button>
+                <div class="dropdown-content">
+                    <a href="/schools">Schools</a>
+                    <a href="/parents">Parents</a>
+                    <a href="/donate">Donate</a>
+                </div>
+            </div>  
+        <a href="/contact">Contact</a>
+    </div>
+    <button onclick="myFunction()">Try it</button>
+    <script>
+    function myFunction() {
+        var x = document.getElementById("myHeader");
+        x.style.color = "red";
+    }
+    </script>
+    <h1> Our Mission </h1>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
         sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
         Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris 
@@ -119,7 +150,5 @@ $(function(){
         reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
         Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia 
         deserunt mollit anim id est laborum.</p>
-
 </body>
 </html>
-
